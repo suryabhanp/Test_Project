fed0003b539a47236c1b05e13061882f2681bfe2
surya pandey <ppand@LAPTOP-7KFKI6MC>
2023-02-09 17:20:48 +0530

    First Commit

---
+++ restassured-fundamentals/src/test/java/com/capita/restassured_fundamentals/MainTest.java
@@ -0,0 +1,49 @@
+package com.capita.restassured_fundamentals;
+
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * A simple unit test
+ */
+public class MainTest 
+{
+	
+	String driverPath = "src/main/resources/";
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	System.out.println("Hello World");
+        assertTrue( true );
+    }
+    
+    @Test
+    public void shouldAnswerWithTrue1()
+    {
+		/*
+		 * WebDriver driver = new ChromeDriver();
+		 * 
+		 * driver.get("https://www.google.com/intl/en-GB/gmail/about/"); driver.close();
+		 */
+    	WebDriverManager.chromedriver().setup();
+    	ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--start-maximized");
+		chromeOptions.addArguments("--diable--notifications");
+		chromeOptions.addArguments("disable-infobars"); 
+    	WebDriver driver = new ChromeDriver();
+    	driver.get("http://google.com");
+    	System.out.println(driver.getTitle());
+    	driver.close();
+    }
+    
+}
